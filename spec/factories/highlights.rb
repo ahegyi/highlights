517f2f974dda7edcd67687b7e73878501faac976
Aron Hegyi <ahegyi@gmail.com>
2013-12-02 00:00:02 -0800

    Add highlight model

---
+++ spec/factories/highlights.rb
@@ -0,0 +1,8 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :highlight do
+    title "Wahoo!"
+    description "A cool place in the world."
+  end
+end
