da1a04fa7f71be2fb35967545a98d89a804f9177
Aron Hegyi <ahegyi@gmail.com>
2013-12-02 00:55:04 -0800

    Add ability to view a user's highlights

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Highlights::Application.routes.draw do
   devise_for :users
 
   resources :highlights, only: [:create, :show, :index, :new]
+  resources :users, only: [:show]
 
   authenticated :user do
     root to: "highlights#index"
